5981dcf02974397a48ec44fc21538b48055eccb9
hecto <hector.antona@gmail.com>
2021-03-16 09:41:46 +0100

    SignUp screen

--- app/src/main/java/com/example/bcnet_app/MainActivity.java
+++ app/src/main/java/com/example/bcnet_app/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.bcnet_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -16,6 +19,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button LoginBtn = (Button)findViewById(R.id.LoginBtn);
+        LoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), MainActivity2.class);
+                //
+                startActivity(startIntent);
+            }
+        });
+
+        Button SignupBtn = (Button)findViewById(R.id.SignupBtn);
+        SignupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), SignUpActivity.class);
+                //
+                startActivity(startIntent);
+            }
+        });
 
     }
 
