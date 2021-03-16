d1e2208e09b4956857d8aa4e4c40a1ee96140535
Adriafu <adria.fuster@estudiantat.upc.edu>
2021-03-16 17:53:10 +0100

    Menú v1

--- app/src/main/java/com/example/bcnet_app/MainActivity2.java
+++ app/src/main/java/com/example/bcnet_app/MainActivity2.java
@@ -1,8 +1,13 @@
 package com.example.bcnet_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +15,27 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.perfil:
+
+                return true;
+
+            case R.id.logout:
+                Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+                startActivity(intent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
