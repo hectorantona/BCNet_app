ecf14536c21f14b3914ee1ec4946aa8bf92e01a1
Marc Crespo <marc.crespo.hernandez@est.fib.upc.edu>
2021-03-17 13:39:53 +0100

    Sign up v1

--- app/src/main/java/com/example/bcnet_app/SignUpActivity.java
+++ app/src/main/java/com/example/bcnet_app/SignUpActivity.java
@@ -2,7 +2,10 @@ package com.example.bcnet_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        Button BtnSignUpContinue = (Button)findViewById(R.id.BtnContinue);
+        BtnSignUpContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), MainActivity2.class);
+                //
+                startActivity(startIntent);
+            }
+        });
     }
 }
