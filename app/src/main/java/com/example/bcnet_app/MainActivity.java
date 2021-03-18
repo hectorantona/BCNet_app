4350ec19a86c1bf76f209d2d36b258ede7d42160
Adriafu <adria.fuster@estudiantat.upc.edu>
2021-03-18 16:30:37 +0100

    Ordenat

--- app/src/main/java/com/example/bcnet_app/MainActivity.java
+++ app/src/main/java/com/example/bcnet_app/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.bcnet_app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextPaint;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -13,24 +17,37 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
+
+
+    EditText user, password;
+    Button LoginBtn, SignupBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button LoginBtn = (Button)findViewById(R.id.LoginBtn);
+        password = findViewById(R.id.password);
+        user = findViewById(R.id.user);
+        SignupBtn = (Button)findViewById(R.id.SignupBtn);
+        LoginBtn = (Button)findViewById(R.id.LoginBtn);
+
+        //click login
         LoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                //Validate inputs
+                String useri = String.valueOf(user.getText());
+                String passwordi = String.valueOf(password.getText());
+
                 Intent startIntent = new Intent(getApplicationContext(), MainActivity2.class);
-                //
                 startActivity(startIntent);
             }
         });
 
-        Button SignupBtn = (Button)findViewById(R.id.SignupBtn);
         SignupBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -39,7 +56,5 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(startIntent);
             }
         });
-
     }
-
 }
--- app/src/main/java/com/example/bcnet_app/ui/UserModelView.java
+++
@@ -1,7 +0,0 @@
-package com.example.bcnet_app.ui;
-
-import androidx.lifecycle.ViewModel;
-
-public class UserModelView extends ViewModel {
-     
-}
