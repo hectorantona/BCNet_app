60834a7ca29c68bd5fed8e04a989413da2e6a07c
hecto <hector.antona@gmail.com>
2021-05-08 14:45:36 +0200

    New Local view

---
+++ app/src/main/java/com/example/bcnet_app/NewLocalActivity.java
@@ -0,0 +1,14 @@
+package com.example.bcnet_app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewLocalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_local);
+    }
+}
