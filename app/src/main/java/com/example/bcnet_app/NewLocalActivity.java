2423f367c7a2b51c836bbb2f39e6282778514a19
Adriafu <adria.fuster@estudiantat.upc.edu>
2021-05-18 18:54:50 +0200

    mapa done sense focus

--- app/src/main/java/com/example/bcnet_app/NewLocalActivity.java
+++ app/src/main/java/com/example/bcnet_app/NewLocalActivity.java
@@ -1,7 +1,12 @@
 package com.example.bcnet_app;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -14,38 +19,66 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class NewLocalActivity extends AppCompatActivity implements OnMapReadyCallback  {
 
     private GoogleMap mapa;
-    private SupportMapFragment mapfrag;
+    private EditText nomloc;
+    private String categoria;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_local);
 
+        nomloc = findViewById(R.id.local_name);
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        creaciomapa();
-
     }
-    private void creaciomapa () {
-        SupportMapFragment mapFragment = SupportMapFragment.newInstance();
-        getSupportFragmentManager()
-                .beginTransaction()
-                .add(R.id.new_local_form, mapFragment)
-                .commit();
 
-        mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.categoria_menu, menu);
+        return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.museu:
+                    categoria = "museu";
+                return true;
+
+            case R.id.restaurant:
+                    categoria = "restaurant";
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mapa = googleMap;
+        //Es fa autozoom a les co
+        LatLng origen = new LatLng(41.3890927,2.1575136);
+        mapa.animateCamera(CameraUpdateFactory.newLatLngZoom(origen, 20));
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mapa.addMarker(new MarkerOptions()
-                .position(sydney)
-                .title("Marker in Sydney"));
-        mapa.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mapa.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                MarkerOptions markerOptions = new MarkerOptions().draggable(true);
+                //Set marker position
+                markerOptions.position(latLng);
+                //Set laltitude and longitude
+                markerOptions.title(nomloc.getText().toString());
+                //Clear the previous click
+                mapa.clear();
+                //zoom the marker
+                mapa.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                //add marker on map
+                mapa.addMarker(markerOptions);
+            }
+        });
+
     }
 }
