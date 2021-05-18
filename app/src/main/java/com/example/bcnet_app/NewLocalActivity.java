ea436ff306362553aa912cda1d249f73dc151671
Adriafu <adria.fuster@estudiantat.upc.edu>
2021-05-18 17:03:37 +0200

    mapa show done

--- app/src/main/java/com/example/bcnet_app/NewLocalActivity.java
+++ app/src/main/java/com/example/bcnet_app/NewLocalActivity.java
@@ -4,11 +4,48 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class NewLocalActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+public class NewLocalActivity extends AppCompatActivity implements OnMapReadyCallback  {
+
+    private GoogleMap mapa;
+    private SupportMapFragment mapfrag;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_local);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        creaciomapa();
+
+    }
+    private void creaciomapa () {
+        SupportMapFragment mapFragment = SupportMapFragment.newInstance();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.new_local_form, mapFragment)
+                .commit();
+
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mapa = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mapa.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney"));
+        mapa.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
